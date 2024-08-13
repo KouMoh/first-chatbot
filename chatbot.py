f528e707eb3ed39b6b33363d6b0e3787ae162f0b
KouMoh <126945657+KouMoh@users.noreply.github.com>
2024-08-13 23:44:46 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,6 @@
 import dotenv
-from langchain_openai import ChatOpenAI
+from langchain_ollama import OllamaLLM
 
 dotenv.load_dotenv()
 
-chat_model = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
+llm = OllamaLLM(model="llama3.1")
